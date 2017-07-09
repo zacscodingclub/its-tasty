fd5c24df64dc80c255086dad8628fbe1c4294138
zacscodingclub <zbaston@gmail.com>
2017-07-09 12:55:18 -0400

    add store

---
+++ controllers/storeController.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homepage = (req, res) => {
+  res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/stores/${store.slug}`);
+};
--- start.js
+++ start.js
@@ -17,8 +17,8 @@ mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
-// READY?! Let's go!
-
+// import all models
+require('./models/Store');
 
 // Start our app!
 const app = require('./app');

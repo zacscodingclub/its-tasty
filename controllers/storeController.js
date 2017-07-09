c94f59a11a45c9b134638bb8e1067f1eadc444f4
zacscodingclub <zbaston@gmail.com>
2017-07-09 14:21:00 -0400

    edit store complete

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -15,3 +15,26 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/stores/${store.slug}`);
 };
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body,
+    {
+      new: true, // returns new store instead of old one, findOneAndUpdate normally returns old store
+      runValidators: true
+  }).exec();
+
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</store>`);
+  res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+
+  res.render('stores', { title: 'Stores', stores });
+}
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+}

60a7a7e7c75046a7446f885f5c4ac2b8337b33a9
zacscodingclub <zbaston@gmail.com>
2017-07-09 16:02:22 -0400

    photo upload working.  store view started

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: "That filetype isn't allowed"}, false);
+    }
+  }
+};
+
 
 exports.homepage = (req, res) => {
   res.render('index');
@@ -9,6 +26,23 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  if (!req.file) {
+    next();
+    return;
+  }
+
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
+}
+
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
 
@@ -17,6 +51,7 @@ exports.createStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point';
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body,
     {
       new: true, // returns new store instead of old one, findOneAndUpdate normally returns old store
@@ -37,4 +72,10 @@ exports.editStore = async (req, res) => {
   const store = await Store.findOne({ _id: req.params.id });
 
   res.render('editStore', { title: `Edit ${store.name}`, store });
+}
+
+exports.getStore = async (req, res) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+
+  res.render('store', { title: `${store.name}`, store });
 }
--- models/Store.js
+++ models/Store.js
@@ -13,7 +13,26 @@ const storeSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-  tags: [String]
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  photo: String,
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates!'}
+    ],
+    address: {
+      type: String,
+      required: 'You must supply an address!'
+    }
+  }
 });
 
 storeSchema.pre('save', function(next) {
@@ -21,7 +40,7 @@ storeSchema.pre('save', function(next) {
     next();
     return;
   }
-  
+
   this.slug = slug(this.name);
   next();
 });

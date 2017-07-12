c61b2f01f153ba5345c364687b3da4cbbac049e9
zacscodingclub <zbaston@gmail.com>
2017-07-12 11:49:30 -0400

    implement pagination

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -65,9 +65,25 @@ exports.updateStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
-  const stores = await Store.find();
+  const page = req.params.page || 1;
+  const limit = 4;
+  const skip = (page * limit) - limit;
 
-  res.render('stores', { title: 'Stores', stores });
+  const storesPromise = Store.
+    find()
+    .skip(skip)
+    .limit(limit)
+    .sort({ created: 'desc' });
+
+  const countPromise = Store.count();
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+  const pages = Math.ceil(count / limit);
+  if (!stores.length && skip) {
+    req.flash('info', `You asked for page ${page}, but that doesn't exist!  So we put you back on ${pages}.`);
+    res.redirect(`/stores/page/${pages}`);
+  }
+  res.render('stores', { title: 'Stores', stores, count, pages, page });
 };
 
 const confirmOwner = (store, user) => {
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
+router.get('/stores/page/:page', catchErrors(storeController.getStores));
 router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/add',
   authenticationController.isLoggedIn,
